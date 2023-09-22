f0b4e225001005065c72f830ee7d08fd5fd63ab1
RichardGarita <richard.garita@ucr.ac.cr>
2023-09-22 15:31:12 -0600

    Se agregan mensajes de error para campos obligatorios

--- client/src/componentes/addPolicy/AddPolicy.js
+++ client/src/componentes/addPolicy/AddPolicy.js
@@ -1,13 +1,46 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 function AddPolicy() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div>
-      <form>
-        <label>
-            Nombre:
-            <input type="text" required></input>
-        </label>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="name">Nombre completo</label>
+        <input
+          {...register("name", {
+            required: "Este campo es obligatorio",
+          })}
+          name="name"
+          type="text"
+        />
+        {errors.name && <span>{errors.name.message}</span>}
+
+        <label htmlFor="startDate">Fecha de Inicio</label>
+        <input
+          {...register("startDate", {
+            required: "Este campo es obligatorio",
+          })}
+          name="startDate"
+          type="date"
+        />
+        {errors.startDate && <span>{errors.startDate.message}</span>}
+
+        <label htmlFor="email">Correo electrónico</label>
+        <input
+          {...register("email", {
+            required: "Este campo es obligatorio",
+          })}
+          name="email"
+          type="email"
+        />
+        {errors.email && <span>{errors.email.message}</span>}
+
         <input type="submit" />
       </form>
     </div>
