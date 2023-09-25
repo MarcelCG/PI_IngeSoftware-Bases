6dd53c49d3d0900da5ae1a9f27b54061e3e70a12
RichardGarita <richard.garita@ucr.ac.cr>
2023-09-25 14:29:31 -0600

    Se agrega CreatePolitica al controlador de politica

--- server/controllers/politicasController.js
+++ server/controllers/politicasController.js
@@ -10,6 +10,48 @@ async function getAllPoliticas(req, res) {
   }
 }
 
+// Crear una nueva política
+async function createPolitica(req, res) {
+  try {
+    const {
+      titulo,
+      cedula_empresa,
+      periodo,
+      fecha_inicio,
+      fecha_final,
+      inicia_desde_contrato,
+      horas_a_dar,
+      incrementativo,
+      acumulativo,
+      activo,
+    } = req.body;
+
+    // Llama a la función createPolitica que inserta en la tabla "Politica"
+    const success = await createPolitica(
+      titulo,
+      cedula_empresa,
+      periodo,
+      fecha_inicio,
+      fecha_final,
+      inicia_desde_contrato,
+      horas_a_dar,
+      incrementativo,
+      acumulativo,
+      activo
+    );
+
+    if (success) {
+      res.status(201).json({ message: 'Política creada exitosamente' });
+    } else {
+      res.status(500).json({ message: 'No se pudo crear la política' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+
 module.exports = {
   getAllPoliticas,
+  createPolitica,
 };
