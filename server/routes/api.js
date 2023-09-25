2673610e39263a917389079abe9a960f9d51c6b9
RichardGarita <richard.garita@ucr.ac.cr>
2023-09-25 14:11:21 -0600

    Se agrega la ruta de politicas al api

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const politicasRoutes = require('./politicasRoutes')
 
+router.use('/politicas', politicasRoutes);
 
 module.exports = router;
