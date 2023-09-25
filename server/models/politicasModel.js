c4e6993b8b18a21c7f3456a806329802f2ebdc1d
RichardGarita <richard.garita@ucr.ac.cr>
2023-09-25 14:27:25 -0600

    Se agrega create al modelo Politicas

--- server/models/politicasModel.js
+++ server/models/politicasModel.js
@@ -12,6 +12,50 @@ async function getAll() {
     }
 }
 
+async function createPolitica(
+  titulo,
+  cedula_empresa,
+  periodo,
+  fecha_inicio,
+  fecha_final,
+  inicia_desde_contrato,
+  horas_a_dar,
+  incrementativo,
+  acumulativo,
+  activo
+) {
+  try {
+    const pool = await sql.connect(dbConfig);
+    const result = await pool
+      .request()
+      .input('titulo', sql.NVarChar, titulo)
+      .input('cedula_empresa', sql.NVarChar, cedula_empresa)
+      .input('periodo', sql.Decimal(5, 2), periodo)
+      .input('fecha_inicio', sql.Date, fecha_inicio)
+      .input('fecha_final', sql.Date, fecha_final)
+      .input('inicia_desde_contrato', sql.Bit, inicia_desde_contrato)
+      .input('horas_a_dar', sql.Decimal(5, 2), horas_a_dar)
+      .input('incrementativo', sql.Bit, incrementativo)
+      .input('acumulativo', sql.Bit, acumulativo)
+      .input('activo', sql.Bit, activo)
+      .query(
+        `INSERT INTO Politica (
+          titulo, cedula_empresa, periodo, fecha_inicio, fecha_final,
+          inicia_desde_contrato, horas_a_dar, incrementativo, acumulativo, activo
+        )
+        VALUES (
+          @titulo, @cedula_empresa, @periodo, @fecha_inicio, @fecha_final,
+          @inicia_desde_contrato, @horas_a_dar, @incrementativo, @acumulativo, @activo
+        )`
+      );
+    return result.rowsAffected > 0;
+  } catch (error) {
+    throw error;
+  }
+}
+
+
 module.exports = {
   getAll,
+  createPolitica,
 };
