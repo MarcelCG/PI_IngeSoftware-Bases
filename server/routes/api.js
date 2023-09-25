9e5a1cdb9390ae1b0218ac4889ab0ee1607ab6e3
RichardGarita <richard.garita@ucr.ac.cr>
2023-09-25 14:33:24 -0600

    comments

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+// Importa rutas
 const politicasRoutes = require('./politicasRoutes')
 
+
+// Asocia las rutas
 router.use('/politicas', politicasRoutes);
 
 module.exports = router;
