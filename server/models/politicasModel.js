aa645980766eae7ea259663d17db506ffe0c5eb3
RichardGarita <richard.garita@ucr.ac.cr>
2023-09-25 14:02:05 -0600

    Se crea modelo de politica con funcion getAll

---
+++ server/models/politicasModel.js
@@ -0,0 +1,13 @@
+const sql = require('mssql');
+const dbConfig = require('../config/dbconfig'); // Importa la configuración de la base de datos
+
+// Obtener todas las Politicas
+async function getAll() {
+    try {
+      const pool = await sql.connect(dbConfig);
+      const result = await pool.request().query('SELECT * FROM Politica');
+      return result.recordset;
+    } catch (error) {
+      throw error;
+    }
+}
