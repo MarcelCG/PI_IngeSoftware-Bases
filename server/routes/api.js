d3da4fa67a0bb8e3d3fb3afbb45415e05fe96992
RichardGarita <richard.garita@ucr.ac.cr>
2023-09-26 21:16:02 -0600

    Implementada funcionalidad de empresa

--- server/routes/api.js
+++ server/routes/api.js
@@ -9,6 +9,7 @@ const jornadaRoutes = require('./jornadaRoutes')
 const libresRoutes = require('./libresRoutes')
 const telefonosEmpresaRoutes = require('./telefonosEmpresaRoutes')
 const correosEmpresaRoutes = require('./correosEmpresasRoutes')
+const EmpresasRoutes = require('./empresaRoutes')
 
 
 // Asocia las rutas
@@ -19,5 +20,6 @@ router.use('/jornadas', jornadaRoutes);
 router.use('/libres', libresRoutes);
 router.use('/telEmpresa', telefonosEmpresaRoutes);
 router.use('/correoEmpresa', correosEmpresaRoutes);
+router.use('/empresa', EmpresasRoutes);
 
 module.exports = router;
