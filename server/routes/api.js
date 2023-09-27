d5e747c7a39d0914051e5b0f4f36fad85c6394f2
RichardGarita <richard.garita@ucr.ac.cr>
2023-09-26 19:50:00 -0600

    Se implementa funcionalidad jornada

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,11 +5,13 @@ const router = express.Router();
 const politicasRoutes = require('./politicasRoutes')
 const empleadoRoutes = require('./empleadoRoutes')
 const solicitudRoutes = require('./solicitudRoutes')
+const jornadaRoutes = require('./jornadaRoutes')
 
 
 // Asocia las rutas
 router.use('/politicas', politicasRoutes);
 router.use('/empleados', empleadoRoutes);
 router.use('/solicitudes', solicitudRoutes);
+router.use('/jornadas', jornadaRoutes);
 
 module.exports = router;
