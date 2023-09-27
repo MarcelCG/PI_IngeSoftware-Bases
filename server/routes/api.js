69af334466de4b8a44401417ec237bd42ec56bb0
RichardGarita <richard.garita@ucr.ac.cr>
2023-09-26 20:02:12 -0600

    Agregado funcionalidad de libres

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,6 +6,7 @@ const politicasRoutes = require('./politicasRoutes')
 const empleadoRoutes = require('./empleadoRoutes')
 const solicitudRoutes = require('./solicitudRoutes')
 const jornadaRoutes = require('./jornadaRoutes')
+const libresRoutes = require('./libresRoutes')
 
 
 // Asocia las rutas
@@ -13,5 +14,6 @@ router.use('/politicas', politicasRoutes);
 router.use('/empleados', empleadoRoutes);
 router.use('/solicitudes', solicitudRoutes);
 router.use('/jornadas', jornadaRoutes);
+router.use('/libres', libresRoutes);
 
 module.exports = router;
