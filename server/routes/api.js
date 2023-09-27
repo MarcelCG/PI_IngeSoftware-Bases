ebc75dbf16941d4b0283d3e7f04db6c9cb339efd
RichardGarita <richard.garita@ucr.ac.cr>
2023-09-26 19:36:36 -0600

    Rutas Actualizadas

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,10 +4,12 @@ const router = express.Router();
 // Importa rutas
 const politicasRoutes = require('./politicasRoutes')
 const empleadoRoutes = require('./empleadoRoutes')
+const solicitudRoutes = require('./solicitudRoutes')
 
 
 // Asocia las rutas
 router.use('/politicas', politicasRoutes);
 router.use('/empleados', empleadoRoutes);
+router.use('/solicitudes', solicitudRoutes);
 
 module.exports = router;
