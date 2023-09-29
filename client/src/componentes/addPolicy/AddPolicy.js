5e358adffd99065eb5da54c2569661f1b0f8609c
RichardGarita <richard.garita@ucr.ac.cr>
2023-09-28 23:03:28 -0600

    Se arreglan los campos para coincidir con la base de datos

--- client/src/componentes/addPolicy/AddPolicy.js
+++ client/src/componentes/addPolicy/AddPolicy.js
@@ -1,68 +1,68 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import { useForm } from "react-hook-form";
 import axios from 'axios';
 import "./AddPolicy.css"
+
+  // URL para el Api
+  const api = 'http://localhost:4223/api';
+
+  // URL para el manejo de politicas
+  const politicas = api + '/politicas';
+
+  // Cedula de la empresa que inició sesión
+  const empresa = '123ABC';
 
 function AddPolicy() {
 
   // Configuración del formulario usando react-hook-form
   const { register, handleSubmit, formState: { errors }, clearErrors } = useForm();
 
-  const [shouldFetchData, setShouldFetchData] = useState(false);
-
-  useEffect(() => {
-    if (shouldFetchData) {
-      // Realiza una solicitud GET al servidor Express
-      axios.get('http://localhost:4223/api/politicas')
-        .then((response) => {
-          console.log(response.data);
-          setShouldFetchData(false); // Establece shouldFetchData nuevamente en false
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }
-  }, [shouldFetchData]);
-
   // Función que se ejecuta al enviar el formulario
   const onSubmit = (data) => {
-    console.log(data);
-
-    setShouldFetchData(true);
-
     const {
       titulo,
       periodo,
+      periodUnit,
       fecha_inicio,
       fecha_final,
       inicia_desde_contrato,
-      horas_a_dar,
+      dias_a_dar,
+      dias_a_darUnit,
       incrementativo,
+      dias_a_incrementar,
+      incrementalUnit,
       acumulativo,
+      descripcion,
     } = data;
+
 
     console.log('Valores extraídos de data:');
         console.log('Titulo:', titulo);
-        console.log('Periodo:', periodo);
-        console.log('Fecha de inicio:', fecha_inicio);
+        console.log('Empresa:', empresa);
+        console.log('Periodo:', periodo * (periodUnit === "1/24" ? (1/24): periodUnit));
+        console.log('Fecha de inicio:', inicia_desde_contrato ? '0000-00-00': fecha_inicio);
         console.log('Fecha final:', fecha_final);
         console.log('Inicia desde contrato:', inicia_desde_contrato);
-        console.log('Horas a dar:', horas_a_dar);
-        console.log('Incrementativo:', incrementativo);
+        console.log('Dias a dar:', dias_a_dar * (dias_a_darUnit === "1/24" ? (1/24): dias_a_darUnit));
+        console.log('Incrementativo:', !incrementativo);
+        console.log('Dias a Incrementar:', dias_a_incrementar * (incrementalUnit === "1/24" ? (1/24): incrementalUnit));
         console.log('Acumulativo:', acumulativo);
+        console.log('Descripcion:', descripcion);
   
     axios
-      .post('http://localhost:4223/api/politicas', {
+      .post(politicas, {
         titulo,
-        cedula_empresa: '33346390',
-        periodo,
-        fecha_inicio,
+        cedula_empresa: empresa,
+        periodo: periodo * (periodUnit === "1/24" ? (1/24): periodUnit),
+        fecha_inicio: inicia_desde_contrato ? '2023-01-01': fecha_inicio,
         fecha_final,
         inicia_desde_contrato,
-        horas_a_dar,
-        incrementativo,
+        dias_a_dar: dias_a_dar * (dias_a_darUnit === "1/24" ? (1/24): dias_a_darUnit),
+        incrementativo: !incrementativo,
+        dias_a_incrementar: incrementativo ? 0 : dias_a_incrementar * (incrementalUnit === "1/24" ? (1/24): incrementalUnit),
         acumulativo,
         activo:true,
+        descripcion,
       })
       .then((response) => {
         console.log('Solicitud POST exitosa:', response.data);
@@ -70,9 +70,6 @@ function AddPolicy() {
       .catch((error) => {
         console.error('Error en la solicitud POST:', error);
       });
-
-
-
   };
   
 
@@ -97,15 +94,15 @@ function AddPolicy() {
         message: "Este campo debe ser mayor a 0"
       },
     },
-    horas_a_dar: {
+    dias_a_dar: {
       pattern: {
         value: /^[1-9]\d*$/,
         message: "Este campo debe ser mayor a 0"
       },
     },
-    monto_a_incrementar: {
+    dias_a_incrementar: {
       pattern: {
-        value: /^[1-9]\d*$/,
+        value: !disableIncremental ? /^[1-9]\d*$/ : '',
         message: "Este campo debe ser mayor a 0"
       },
     },
@@ -148,7 +145,10 @@ function AddPolicy() {
             {...register("inicia_desde_contrato")}
             type="checkbox" 
             checked={disableStartDate} 
-            onChange={(e) => {setDisableStartDate(e.target.checked); clearErrors("fecha_inicio")}}
+            onChange={(e) => {
+              setDisableStartDate(e.target.checked);
+              clearErrors("fecha_inicio");
+            }}
           />
 
           <label>Rige a partir del contrato</label>
@@ -182,25 +182,25 @@ function AddPolicy() {
           <select className="drop"
             {...register("periodUnit")}
           >
-            <option value="horas">Horas</option>
-            <option value="días">Días</option>
-            <option value="semanas">Semanas</option>
-            <option value="meses">Meses</option>
-            <option value="años">Años</option>
+            <option value="1/24" selected>Horas</option>
+            <option value="1">Días</option>
+            <option value="7">Semanas</option>
+            <option value="30">Meses</option>
+            <option value="365">Años</option>
           </select>
 
           {errors.periodo && <span className="mensjError">{errors.periodo.message}</span>}
         </section>
 
         {/* Campo de incrementativo*/}
-        <label className="etiqueta" htmlFor="monto_a_incrementar">Incremento por Periodo: </label>
+        <label className="etiqueta" htmlFor="dias_a_incrementar">Incremento por Periodo: </label>
         <section className="campoDrop">
-          <input className={`campo ${errors.monto_a_incrementar ? "campoError" : ""}`}
-            {...register("monto_a_incrementar", {
+          <input className={`campo ${errors.dias_a_incrementar ? "campoError" : ""}`}
+            {...register("dias_a_incrementar", {
               required: !disableIncremental ? errorMessages.required : false,
-              ...validationPatterns.monto_a_incrementar
+              ...validationPatterns.dias_a_incrementar
             })}
-            name="monto_a_incrementar"
+            name="dias_a_incrementar"
             type="number"
             disabled={disableIncremental}
             min={0}
@@ -212,13 +212,13 @@ function AddPolicy() {
               required: !disableIncremental ? errorMessages.required : false
             })}
           >
-            <option value="horas">Horas</option>
-            <option value="días">Días</option>
-            <option value="semanas">Semanas</option>
-            <option value="meses">Meses</option>
-            <option value="años">Años</option>
+            <option value="1/24" selected>Horas</option>
+            <option value="1">Días</option>
+            <option value="7">Semanas</option>
+            <option value="30">Meses</option>
+            <option value="365">Años</option>
           </select>
-          {errors.monto_a_incrementar && <span className="mensjError">{errors.monto_a_incrementar.message}</span>}
+          {errors.dias_a_incrementar && <span className="mensjError">{errors.dias_a_incrementar.message}</span>}
         </section>
 
         {/* Checkbox para "Incrementativo" */}
@@ -227,36 +227,36 @@ function AddPolicy() {
             {...register("incrementativo")}
             type="checkbox" 
             checked={disableIncremental} 
-            onChange={(e) => {setDisableIncremental(e.target.checked); clearErrors("monto_a_incrementar");}}
+            onChange={(e) => {setDisableIncremental(e.target.checked); clearErrors("dias_a_incrementar");}}
           />
 
           <label>No es incrementativo</label>
         </section>
 
         {/*Campo para la unidad*/}
-        <label className="etiqueta" htmlFor="horas_a_dar">Unidad: </label>
+        <label className="etiqueta" htmlFor="dias_a_dar">Unidad: </label>
         <section className="campoDrop">
-          <input className={`campo ${errors.horas_a_dar ? "campoError" : ""}`}
-            {...register("horas_a_dar", {
+          <input className={`campo ${errors.dias_a_dar ? "campoError" : ""}`}
+            {...register("dias_a_dar", {
               required: errorMessages.required,
-              ...validationPatterns.horas_a_dar,
+              ...validationPatterns.dias_a_dar,
             })}
-            name="horas_a_dar"
+            name="dias_a_dar"
             type="number"
             min={0}
             defaultValue={0}
           />
 
           <select className="drop"
-            {...register("horas_a_darUnit")}
+            {...register("dias_a_darUnit")}
           >
-            <option value="horas">Horas</option>
-            <option value="días">Días</option>
-            <option value="semanas">Semanas</option>
-            <option value="meses">Meses</option>
-            <option value="años">Años</option>
+            <option value='1/24' selected>Horas</option>
+            <option value="1">Días</option>
+            <option value="7">Semanas</option>
+            <option value="30">Meses</option>
+            <option value="365">Años</option>
           </select>
-          {errors.horas_a_dar && <span className="mensjError">{errors.horas_a_dar.message}</span>}
+          {errors.dias_a_dar && <span className="mensjError">{errors.dias_a_dar.message}</span>}
         </section>
 
         {/* Checkbox para "Acumulativo" */}
