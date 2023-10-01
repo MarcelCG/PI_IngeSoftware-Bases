c3c7341af5b6a5c03a8bebb3492f9864b28c699b
Jeremy <jeremy@kali.eclypze.com>
2023-10-01 16:19:06 -0600

    Pagina principal para anhadir Views

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import {ViewPoliticas} from './componentes/viewPoliticas'
+import AddPolicy from './componentes/addPolicy/AddPolicy'
 
 // import Inicio from './Inicio';
 // import Empleados from './Empleados';
 // import Politicas from './Politicas';
 
-function App() {
+function App({cedula}) {
+
   const [menuVisible, setMenuVisible] = useState(false);
 
   const toggleMenu = () => {
@@ -15,31 +20,43 @@ function App() {
 
   return (
     <Router>
-      <div className="App">
-        <header>
-          <button onClick={toggleMenu}>Mostrar/ocultar menú</button>
-        </header>
+      <div className="bg-fondo p-3" >
+       <div className="App bg-fondo" >
+        <button onClick={toggleMenu}>Mostrar/ocultar menú</button>
         <main>
-          <div className={`menu ${menuVisible ? 'visible' : ''}`}>
-            <ul>
-              <li>
-                <Link to="/">Inicio</Link>
-              </li>
-              <li>
-                <Link to="/empleados">Empleados</Link>
-              </li>
-              <li>
-                <Link to="/politicas">Políticas</Link>
-              </li>
-            </ul>
-          </div>
-          <div className="content">
-            {/* Rutas correspondientes a los componentes */}
-            {/* <Route path="/" exact component={Inicio} /> */}
-            {/* <Route path="/empleados" component={Empleados} /> */}
-            {/* <Route path="/politicas" component={Politicas} /> */}
+          <div>
+            <div className={`menu ${menuVisible ? 'visible' : ''}`}>
+              <ul>
+                <li>
+                  <Link to="/">Inicio</Link>
+                </li>
+                <li>
+                  <Link to="/empleados">Empleados</Link>
+                </li>
+                <li>
+                  <Link to="/politicas">Políticas</Link>
+                </li>
+                <li>
+                  <Link to="/AddPoliticas">Anhadir Politica</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="content">
+            </div>
           </div>
         </main>
+        <div className="contenedor p-2" style={{ overflowY: 'auto', maxHeight: '100vh' }}>
+          <Routes>
+               {/*<Route path="/politicas" element={<ViewPoliticas items={items}/>} />*/}
+               {/*<Route path="/AddPoliticas" element={<AddPolicy />} />*/}
+          </Routes>
+        </div>
+        <footer style={{ backgroundColor: '#20212a', color: '#ffffff'    }}  >
+        <div className="container">
+          <p class="text-center">&copy; Oraculo.com</p>
+        </div>
+      </footer>
+      </div>
       </div>
     </Router>
   );
