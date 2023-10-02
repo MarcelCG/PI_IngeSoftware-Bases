bc58e76f5693d39424007fc6a8082d5241db04fc
Alejandro Jimenez Corea <pichicareto24@gmail.com>
2023-10-02 12:46:07 -0600

    Se modifico el metodo de cargarDiasLibres para trabajar con un parametro

--- client/src/DiasLibresUsuario.js
+++ client/src/DiasLibresUsuario.js
@@ -3,36 +3,37 @@ import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstr
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 
-function DiasLibresUsuario() {
+function DiasLibresUsuario({ cedulaUsuario }) { // Recibe la cédula del usuario como parámetro
   const [politicas, setPoliticas] = useState([]);
   const [totalDiasLibres, setTotalDiasLibres] = useState(0);
 
-  const cargarDiasLibres = async () => {
-    try {
-      const response = await fetch('http://localhost:4223/api/libres/byEmpleado/123456789', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      if (response.status === 200) {
-        const data = await response.json();
-        setPoliticas(data);
-        const totalDias = data.reduce((total, politica) => total + politica.dias_libres_disponibles, 0);
-        setTotalDiasLibres(totalDias);
-      } else {
-        throw new Error('Error al cargar días libres');
-      }
-    } catch (error) {
-      console.error('Error al cargar días libres:', error);
-    }
-  };
+  
 
   // Llama a la función para cargar los datos de días libres cuando se carga el componente
   useEffect(() => {
+    const cargarDiasLibres = async () => {
+      try {
+        const response = await fetch(`http://localhost:4223/api/libres/byEmpleado/${cedulaUsuario}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+  
+        if (response.status === 200) {
+          const data = await response.json();
+          setPoliticas(data);
+          const totalDias = data.reduce((total, politica) => total + politica.dias_libres_disponibles, 0);
+          setTotalDiasLibres(totalDias);
+        } else {
+          throw new Error('Error al cargar días libres');
+        }
+      } catch (error) {
+        console.error('Error al cargar días libres:', error);
+      }
+    };
     cargarDiasLibres();
-  }, []);
+  }, [cedulaUsuario]); // Agrega cedulaUsuario como dependencia
 
   return (
     <div className="container col-5 align-middle position-static">
@@ -54,4 +55,3 @@ function DiasLibresUsuario() {
 }
 
 export default DiasLibresUsuario;
-
