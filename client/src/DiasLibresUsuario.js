cd13d5b1ebf8a09157ee53865ba8b34851be3f43
Alejandro Jimenez Corea <pichicareto24@gmail.com>
2023-10-02 04:20:19 -0600

    se conecto el backend

--- client/src/DiasLibresUsuario.js
+++ client/src/DiasLibresUsuario.js
@@ -2,41 +2,48 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
- 
-function DiasLibresUsuario() {
-  const [politicas,setDatosDeEmpresa] = useState([]);
 
-  const cargarDiasLibres = () => {
-    // Simulación de carga de datos del empleado
-    // Eventualmente debe ser cambiado para traer los datos de la base de datos
-    const diasLibres = [
-      { nombre: 'Política 1', dias: 1 },
-      { nombre: 'Política 2', dias: 2 },
-      { nombre: 'Política 3', dias: 3 },
-    ];
-    setDatosDeEmpresa(diasLibres);
+function DiasLibresUsuario() {
+  const [politicas, setPoliticas] = useState([]);
+  const [totalDiasLibres, setTotalDiasLibres] = useState(0);
+
+  const cargarDiasLibres = async () => {
+    try {
+      const response = await fetch('http://localhost:4223/api/libres/byEmpleado/123456789', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.status === 200) {
+        const data = await response.json();
+        setPoliticas(data);
+        const totalDias = data.reduce((total, politica) => total + politica.dias_libres_disponibles, 0);
+        setTotalDiasLibres(totalDias);
+      } else {
+        throw new Error('Error al cargar días libres');
+      }
+    } catch (error) {
+      console.error('Error al cargar días libres:', error);
+    }
   };
 
-// Llama a la función para cargar los datos del empleado cuando se carga el componente (Base de datos)
-useEffect(() => { cargarDiasLibres();}, []);
-
-
-
-  const totalDiasLibres = politicas.reduce((total, politica) => total + politica.dias, 0);
-
-
+  // Llama a la función para cargar los datos de días libres cuando se carga el componente
+  useEffect(() => {
+    cargarDiasLibres();
+  }, []);
 
   return (
     <div className="container col-5 align-middle position-static">
       <div className="card shadow m-3">
-        <Card.Header>Total de Vacaciones</Card.Header>
         <Card.Body>
           <Card.Title>Total de Vacaciones: {totalDiasLibres} días</Card.Title>
           <Card.Text>Desglose de Políticas:</Card.Text>
           <ListGroup>
             {politicas.map((politica) => (
-              <ListGroup.Item key={politica.nombre}>
-                {politica.nombre}: {politica.dias} día(s)
+              <ListGroup.Item key={politica.titulo_politica}>
+                {politica.titulo_politica}: {politica.dias_libres_disponibles} día(s)
               </ListGroup.Item>
             ))}
           </ListGroup>
@@ -47,3 +54,4 @@ useEffect(() => { cargarDiasLibres();}, []);
 }
 
 export default DiasLibresUsuario;
+
