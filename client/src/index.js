30b8285c8281c86a61df68138ef92d583a794b56
RichardGarita <richard.garita@ucr.ac.cr>
2023-10-13 22:25:23 -0600

    Se modifican el App y el Index para trabajar con el contexto de autenticacion

--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,34 @@
 import App from './vistas/App'
-import React, {useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Login from './componentes/Registro/Login';
 import Registrarse from './componentes/Registro/Registrarse';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { ProveedorAutenticacion, useAutent } from './contexto/ContextoAutenticacion';
 
 const root = document.getElementById('root');
-
 function Index(){
-
-  const [isLoggedIn, setLoggedIn] = useState(false);
-  const [cedula_usuario, setCedula_Usuario] = useState("");
 
   return(
     <React.StrictMode>
-      <Router>
-        <Routes>
-          <Route path="/app/*" element={isLoggedIn ? <App cedula_usuario={cedula_usuario}/> : <Navigate to="/" />} />
-          <Route path="/" element={<Login setLoggedIn={setLoggedIn} setCedula_Usuario={setCedula_Usuario}/>} />
-          <Route path="/registrarse" element={<Registrarse/>} />
-        </Routes>
-      </Router>
+      <ProveedorAutenticacion>
+        <Router>
+          <IniciarSesion/>
+        </Router>
+      </ProveedorAutenticacion>
     </React.StrictMode>
   );
+}
+
+export function IniciarSesion(){
+  const { logeado } = useAutent();
+  return (
+    <Routes>
+      <Route path="/app/*" element={logeado ? <App/> : <Navigate to="/" />} />
+      <Route path="/" element={<Login/>} />
+      <Route path="/registrarse" element={<Registrarse/>} />
+    </Routes>
+  )
 }
 
 ReactDOM.createRoot(root).render(<Index />);
