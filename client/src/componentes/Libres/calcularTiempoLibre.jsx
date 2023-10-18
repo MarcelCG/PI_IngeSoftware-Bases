3be3a8e085bdb9db6e0c743ba3f7d3c585cafeae
Jeremy <jeremy@kali.eclypze.com>
2023-10-18 10:57:30 -0600

    avace3

--- client/src/componentes/Libres/calcularTiempoLibre.jsx
+++ client/src/componentes/Libres/calcularTiempoLibre.jsx
@@ -1,20 +1,18 @@
-import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import 'react-toastify/dist/ReactToastify.css';
 import {PoliticasData, LibresData} from './data';
-/*
--	CREAR UN TRIGGER QUE CADA VEZ QUE SE CREE UNA POLITICA SE TIENE QUE 
-CREAR LIBRES PARA TODOS LOS EMPLEADOS DE LA EMPRESA DEACUERDO A ESA
-POLITICA
+import React, { useEffect, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 
-TRAER SOLO LAS POLITICAS ACTIVAS
-*/
+/*CREAR UN TRIGGER QUE CADA VEZ QUE SE CREE UNA POLITICA SE TIENE QUE 
+CREAR LIBRES PARA TODOS LOS EMPLEADOS DE LA EMPRESA DEACUERDO A ESA
+POLITICA*/
 
 export const CalcTiemposBoton = () => {
 	const empresa = "cedula_empresa"; 
   const esEmpleador = true;
-  const [Politicas, setPoliticas] = useState(PoliticasData);
-  const [Libres, setLibres] = useState(LibresData);
-  const [Cargando, setCargando] = useState(false);
+  const [Politicas, setPoliticas] = useState([]);
+  const [Libres, setLibres] = useState([]);
 
   // const enviarDatos = async() => {
   // 	try {
@@ -31,75 +29,82 @@ export const CalcTiemposBoton = () => {
   // 		}
   // 	}
 
-	// const cargarDatos = async() => {
-	//   try {
-	//     const respuesta = await axios.get
-	//     (
-	//     	`http://localhost:5000/api/calcularTiempos/${empresa}`
-	//     );
-	//     //setPoliticas(...Politicas,respuesta[0]);
-	//     //setLibres(...Libres,respuesta[1]);
-	//     setCargando(true);
-	//   } catch (error) {
-	//   	setCargando(true);
-	//   }
-	// };
+	const cargarDatos = async() => {
+	  try {
+	    const respuesta = await axios.get
+	    (`http://localhost:5000/api/libres/getLibPol/${empresa}`,);
+	    setPoliticas(respuesta.data.politicas);
+	    setLibres(respuesta.data.libres);
+	    console.log("hola")
+	  } catch (error) {
+
+	  }
+	};
 
 	const Gauss = (n) => {
   	return (n * (n + 1)) / 2;
 	};
 
-	//const fechaHoy = new Date(); // Crear una instancia de Date en lugar de usar Date.now()
+	//const fechaHoy = new Date();
 	const fechaHoy = new Date("2023-11-01");
 	const fechaUnMesAtras = new Date(fechaHoy);
 	fechaUnMesAtras.setMonth(fechaHoy.getMonth() - 1);
-	const milisegHora = 3600000*24;
-	let nuevasHoras = [];
-	const calcularTiempos = () => {
-		//cargarDatos(); // esto tengo que corregirlo
-		// si no tengo un query que me traiga las politicas activas
-		// debo modificar esto, NO TRAER VIGENTES
+	const milisegDia = 86400000; // milisegundos en un dia
+	let nuevosDias = [];
 
-		/*cuidado, fijarme que solo traingan las politicias/libres de la misma empresa*/
-		Politicas.filter(pol => new Date(pol.fecha_final) >= fechaUnMesAtras).
+	const calcularTiempos = async () => {
+		await cargarDatos(); // no traer vigentes
+
+		Politicas.filter(pol => 
+			new Date(pol.fecha_final) >= fechaUnMesAtras &&
+			new Date(pol.fecha_inicio) <= fechaHoy).
 			forEach(pol => {
 				pol.fecha_final = new Date(pol.fecha_final);
+
 				Libres.filter(lib => lib.titulo_politica === pol.titulo).
 					forEach(lib => {
-					
+				
 					const ultimaAct = (fechaHoy > pol.fecha_final) ?
 						pol.fecha_final:fechaHoy;
 
 					const cantPeriodos = 
-						((ultimaAct - fechaUnMesAtras) / milisegHora)/pol.periodo;
+						((ultimaAct - fechaUnMesAtras) / milisegDia)/pol.periodo;
 
-					let nuevosDias = pol.acumulativo ?
+					let newDays = pol.acumulativo ?
 						lib.dias_libres_disponibles +
 						(pol.dias_a_dar * cantPeriodos):
 						pol.dias_a_dar;
 
-					// ocupo tener una variable que me diga la cantida de periodos
-					// que han pasado desde la ultima actualizacion
-					// Aplicar Gauss para el calculo de dias que le corresponden
-					//	por los dias incrementativos
 					if(pol.incrementativo){
-						nuevosDias += 
-						pol.dias_a_incrementar * Gauss(cantPeriodos-1) - Gauss(lib.periodosRecorridos);
+						newDays += 
+						pol.dias_a_incrementar * 
+						( Gauss((cantPeriodos - 1) + lib.periodosRecorridos) -
+						Gauss(cantPeriodos - 1) );
 					}
 
 				  const libre = {
-				  	cedEmple: cantPeriodos,
+				  	cedEmple: lib.cedula_empleado,
 				  	tituloPol: lib.titulo_politica,
-				  	horas: nuevosDias
+				  	Dias: newDays
 					};
-					nuevasHoras.push(libre);
+				
+					nuevosDias.push(libre);
 			});
 		});
-		console.log(nuevasHoras);
+		const numEmpleados = nuevosDias.filter((item, indice, self) =>
+				self.findIndex((elemento) =>
+					elemento.cedEmple === item.cedEmple) === indice
+			).length;
+
+		toast.success(`Empleados actualizados: ${numEmpleados}`,
+			{position: toast.POSITION.TOP_CENTER,
+			className:"alert alert-success"});
+		
 	};
 
 	return (
 		<div>
+			<ToastContainer autoClose={2500}/>
 			<button onClick={calcularTiempos}>
 				hola mundo
 			</button >
@@ -107,9 +112,3 @@ export const CalcTiemposBoton = () => {
 	);
 
 };
-
-
-/*
-
-
-*/
