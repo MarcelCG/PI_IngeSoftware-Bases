b19493f732f90e84ff5198bd966609675fa27910
Jeremy <jeremy@kali.eclypze.com>
2023-11-01 07:15:23 -0600

    correo avance

--- server/servicios/empresaServicios/empresaServicios.js
+++ server/servicios/empresaServicios/empresaServicios.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const CorreoServicio = require('../correoServicios/correoServicios');
 const EmpresaModel = require('../../models/empresaModel/empresasModel');
+const EmpleModel = require('../../models/usuarioModel/Empleado/empleadoModel');
+
 
 async function editarEmpresa(empresa) {
   const estadoEdicion = await EmpresaModel.editarEmpresa(empresa);
@@ -8,8 +11,21 @@ async function editarEmpresa(empresa) {
 };
 
 async function borrarEmpresa(cedula_juridica) {
+  const Empleados = await EmpleModel.getAllByEmpresa(cedula_empresa);
   const estado = await EmpresaModel.borrarEmpresa(cedula_juridica);
+  if (estado === true) {
+
+  }
   return estado;
+};
+
+
+//plantillaHTML, datos, direccion, asunto
+async function enviarCorreoTerminacion(cedula_juridica, Empleados){
+  for (var i = 0; i < Empleados; ++i) {
+    CorreoServicio.enviarCorreo(plantillaHTML, datos, direccion, asunto);
+  }
+
 };
 
 module.exports = {
