548252e514d1afeb61f5b33209ad7da5a4f10da3
Jeremy <jeremy@kali.eclypze.com>
2023-11-11 15:17:32 -0600

    ahora si

--- client/src/componentes/Utiles/Reportes/FiltrosLista.jsx
+++ client/src/componentes/Utiles/Reportes/FiltrosLista.jsx
@@ -5,7 +5,7 @@ import {FiltroPlantilla, AplicarFiltros} from './Filtro'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch, faBroom } from '@fortawesome/free-solid-svg-icons'
 
-export default function FiltrosLista ({rep, filtros, setRep, originales}) {
+export default function FiltrosLista ({rep, filtros, setRep, cargando, originales}) {
 
 	const [filtroCampo, setFiltroCampo] = useState({});
 
@@ -37,10 +37,10 @@ export default function FiltrosLista ({rep, filtros, setRep, originales}) {
 			      	</div>
 			      ))}
 				      <div className='d-flex row justify-content-end'>
-				      	<button className='btn btn-outline-danger btn-lg col-2 m-2' onClick={filtrar}>
+				      	<button className={`btn btn-outline-danger btn-lg col-2 m-2 ${cargando?'':'disabled'}`} onClick={filtrar}>
 				      		Limpiar<FontAwesomeIcon icon={faBroom} />
 				      	</button>
-				      	<button className='btn btn-outline-info btn-lg col-2 m-2' onClick={filtrar}>
+				      	<button className={`btn btn-outline-info btn-lg col-2 m-2 ${cargando?'':'disabled'}`} onClick={filtrar}>
 				      		Buscar<FontAwesomeIcon icon={faSearch} />
 				      	</button>
 				      </div>
