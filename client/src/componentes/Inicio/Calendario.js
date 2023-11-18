09c1aaf6d339a1478a0810824e6ee8411fbba5b1
RichardGarita <richard.garita@ucr.ac.cr>
2023-11-18 14:43:08 -0600

    Mejorada funcionalidad del frente

--- client/src/componentes/Inicio/Calendario.js
+++ client/src/componentes/Inicio/Calendario.js
@@ -1,10 +1,15 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import { Calendar, momentLocalizer } from 'react-big-calendar'
+import { URLApi } from "../Compartido/Constantes";
+import { useAutent } from "../../contexto/ContextoAutenticacion";
+import { Modal } from "../Utiles/Modal";
 import moment from 'moment'
+import axios from 'axios';
 import 'moment/locale/es';
 import "react-big-calendar/lib/css/react-big-calendar.css"
 
 const localizador = momentLocalizer(moment);
+const URLFechasSolicitudes = URLApi + 'solicitudes/fechasAprobadas'
 
 const etiquetas = {
     today: 'Hoy',
@@ -18,13 +23,16 @@ const etiquetas = {
     time: 'Hora',
     event: 'Empleado Libre',
     noEventsInRange: 'No hay empleados libres en este rango.',
+    allDay: 'todo el día',
     showMore: total => `+${total} más`,
 };
 
 const formatos = {
+    // Formato para el día en la sección semana
     dayFormat: (fecha, cultura, localizador) =>
         localizador.format(fecha, 'dddd', cultura).charAt(0).toUpperCase() +
         localizador.format(fecha, 'dddd D', cultura).slice(1),
+    // Formato para el día en la sección mes
     weekdayFormat: (dia, cultura, localizador) =>
         localizador.format(dia, 'dddd', cultura).charAt(0).toUpperCase() +
         localizador.format(dia, 'dddd', cultura).slice(1),
@@ -50,33 +58,93 @@ const formatos = {
         localizador.format(fecha, 'D ', cultura) +
         localizador.format(fecha, 'MMMM', cultura).charAt(0).toUpperCase() +
         localizador.format(fecha, 'MMMM YYYY', cultura).slice(1),
+        
 };
 
-function Calendario() {
-    const fechaHoy = new Date();
+const darFormatoUTC = (fecha) => {
+    fecha = new Date(fecha);
+    var año = fecha.getUTCFullYear();
+    var mes = fecha.getUTCMonth();
+    var dia = fecha.getUTCDate();
+    var horas = fecha.getUTCHours();
+    var minutos = fecha.getUTCMinutes();
 
-    // Crea un evento de ejemplo con la fecha actual
-    const eventos = [
-        {
-            nombreEmpleado: 'Juan Miguel',
-            inicio: fechaHoy,
-            final: fechaHoy,
-        },
-    ];
+    return new Date(año, mes, dia, horas, minutos, 0);
+}
+
+function Calendario() {
+    const {usuarioAutenticado} = useAutent(); 
+    const empresa = usuarioAutenticado.cedula_empresa;
+
+    const modalID = "modalEvento";
+    const botonRef = useRef(null);
+    const [eventoValores, setEventoValores] = useState({
+        titulo: "",
+        componente: "",
+        modalID,
+      });
+
+    const [eventos, setEventos] = useState([]);
+
+    useEffect(() => {
+        const obtenerEventos = async () => {
+            axios.get(`${URLFechasSolicitudes}/${empresa}`)
+                .then((respuesta) => {
+                    respuesta = respuesta.data;
+
+                    const eventosMapeados = respuesta.map(evento => ({
+                        ...evento,
+                        fecha_inicio: darFormatoUTC(evento.fecha_inicio),
+                        fecha_final: darFormatoUTC(evento.fecha_final),
+                      }));
+                    setEventos(eventosMapeados);
+                })
+                .catch((error) => {
+                    if (error.response && error.response.status === 405) {
+                        console.log('No se encontraron eventos');
+                    } else {
+                        console.error('Error al obtener las solicitudes: ', error);
+                    }
+                })
+        };
+
+        obtenerEventos();
+    }, []);
+
+    const abrirModalEvento = (evento) => {
+        console.log(evento);
+        setEventoValores({
+            titulo: "Empleado Libre",
+            componente:
+                <div>
+                    <strong>Nombre Empleado: </strong>{evento.nombre_empleado}<br/>
+                    <strong>Comentarios: </strong> {evento.comentarios}
+                </div>,
+            modalID,
+            tituloEstilos: 'titulo-ventana',
+        });
+        botonRef.current.click();
+    }
 
     return (
-        <div>
+        <>
+        <div ref={botonRef} 
+	      data-bs-toggle="modal" data-bs-target={`#${eventoValores.modalID}`}/>
+          <Modal {...eventoValores}/>
+        <div className="mt-3 mb-3">
             <Calendar
             localizer={localizador}
             events={eventos}
-            startAccessor="inicio"
-            endAccessor="final"
-            titleAccessor="nombreEmpleado"
+            startAccessor="fecha_inicio"
+            endAccessor="fecha_final"
+            titleAccessor="nombre_empleado"
             messages={etiquetas}
             formats={formatos}
+            onSelectEvent={evento => abrirModalEvento(evento)}
             style={{ minHeight: 500 }}
             />
         </div>
+        </>
     )
 }
 
