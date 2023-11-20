a2d9f99650f2d2a5f0e95acda7685a0663620802
JOSE ULISES FONSECA HURTADO <jose.fonsecahurtado@ucr.ac.cr>
2023-11-19 20:36:42 -0600

    Se agrega funcionalidad de frontend del dashboard del empleado

--- client/src/componentes/Dashboard/DashboardEmpleado.js
+++ client/src/componentes/Dashboard/DashboardEmpleado.js
@@ -1,15 +1,37 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { useAutent } from '../../contexto/ContextoAutenticacion';
 import { DashboardEmpleadoHTML } from './DashboardEmpleadoHTML';
+import { URLApi } from '../Compartido/Constantes';
+
+const infoEmpleadoURI = URLApi + 'libres/obtenerInfoLibresPorPolitica/';
 
 function DashboardEmpleado() {
   const {usuarioAutenticado} = useAutent();
   const cedulaEmpleado = usuarioAutenticado.cedula;
-  const cedulaEmpresa = usuarioAutenticado.cedula_empresa;
+  
+  useEffect(() => {
+    const infoPoliticasEmpleado = infoEmpleadoURI + cedulaEmpleado;
+    const obtenerInfoLibresPorPolitica = async () => {
+        try {
+            const respuesta = await axios.get(infoPoliticasEmpleado);
+            setPoliticas(respuesta.data);
+        } 
+        catch (error) {
+            console.error('Error al obtener las politicas:', error);
+        }
+    };
+    obtenerInfoLibresPorPolitica();
+  
+  }, [cedulaEmpleado]);
+  const [politicas, setPoliticas] = useState([]);
+  console.log(politicas);
+
+  let props = {politicas}
 
   return (
     <div className="container mt-2 card-body p-2">
-      <DashboardEmpleadoHTML></DashboardEmpleadoHTML>
+      <DashboardEmpleadoHTML {...props}/>
     </div>
   );
 }
--- client/src/componentes/Dashboard/DashboardEmpleadoHTML.js
+++ client/src/componentes/Dashboard/DashboardEmpleadoHTML.js
@@ -1,47 +1,89 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faChevronLeft, faChevronRight }
+ from '@fortawesome/free-solid-svg-icons'
 import React, {useState} from "react";
 
-export const DashboardEmpleadoHTML = () => {
+export const DashboardEmpleadoHTML = (props) => {
+	const { politicas } = props;
+  
+  let totalDiasDisponibles = 0;
+  politicas.forEach(politica => {
+    totalDiasDisponibles +=
+         politica.dias_libres_disponibles;
+  });
+
+  const [paginaActual, setPaginaActual] = useState(1);
+  const politicasPorPagina = 6;
+  const ultimoIndice = paginaActual * politicasPorPagina;
+  const primerIndice = ultimoIndice - politicasPorPagina;
+  const politicasPagina = politicas.slice(primerIndice, ultimoIndice);
+  const numPag = Math.ceil(politicas.length/politicasPorPagina);
+  const numeros = [...Array(numPag +1).keys()].slice(1)
+
+  function paginaAtras() {
+      if(paginaActual !== 1) {
+          setPaginaActual(paginaActual-1)
+      }
+  }
+  function cambiarPagina(id) {
+      setPaginaActual(id)
+  }
+  function siguientePagina() {
+      if(paginaActual !== numPag) {
+          setPaginaActual(paginaActual+1)
+      }
+  }
 
   return (
-    <div className="row row-cols-1 row-cols-md-3 g-4">
-      <div className="col">
-        <div className="card">
-          <img src="..." className="card-img-top" alt="..."></img>
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          </div>
-        </div>
+    <>
+      <div className='mb-4'>
+        <h5>Total de dias disponibles: {totalDiasDisponibles}</h5>
       </div>
-      <div className="col">
-        <div className="card">
-          <img src="..." className="card-img-top" alt="..."></img>
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        {politicasPagina.map( (politica, index) => (
+          <div className="col">
+            <div className="card">
+              <div className='card-header'>
+                <h5 className='card-title mt-1'>{politica.titulo_politica}</h5>
+              </div>
+              <div className="card-body">
+                  <tr>Dias disponibles: {politica.dias_libres_disponibles}</tr>
+                  <hr/>
+                  <tr>Dias pendientes
+                    de aprobación: {politica.dias_pendientes_aprobacion}</tr>
+                  <hr/>
+                  <tr>Dias próximos
+                    a utilizar: {politica.dias_proximos_utilizar}</tr>
+              </div>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
-      <div className="col">
-        <div className="card">
-          <img src="..." className="card-img-top" alt="..."></img>
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-          </div>
-        </div>
-      </div>
-      <div className="col">
-        <div className="card">
-          <img src="..." className="card-img-top" alt="..."></img>
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          </div>
-        </div>
-      </div>
-    </div>
+      {politicas.length > 6 ? (
+        <nav className='mt-4'>
+          <ul className="pagination">
+              <li className="page-item">
+                  <button className="page-link"
+                      onClick={paginaAtras}><FontAwesomeIcon icon={faChevronLeft} /></button>
+              </li>
+              {
+                  numeros.map((n, i) => (
+                      <li className={`page-item ${paginaActual === n ? 'active' : ''}`} key={i}>
+                          <a href="#" className='page-link'
+                          onClick={()=> cambiarPagina(n)}>{n}</a>
+                      </li>
+                  ))
+              }
+              <li className="page-item">
+              <button className="page-link"
+                  onClick={siguientePagina}><FontAwesomeIcon icon={faChevronRight} /></button>
+              </li>
+          </ul>
+        </nav>
+      ) : (<></>)
+      }
+    </>
 	);
 }
