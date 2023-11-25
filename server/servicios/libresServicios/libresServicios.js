b057fe113c483495fb597d92086c6dc8af527f28
Jeremy <jeremy@kali.eclypze.com>
2023-11-25 16:39:27 -0600

    cambios para anhaddir bitacoras

--- server/servicios/libresServicios/libresServicios.js
+++ server/servicios/libresServicios/libresServicios.js
@@ -3,17 +3,16 @@ const router = express.Router();
 const LibModel = require('../../models/libresModel/libresModel');
 const PolModel = require('../../models/politicaModel/politicasModel');
 const EmpleModel = require('../../models/usuarioModel/Empleado/empleadoModel');
+const BitLibModel = require('../../models/bitacoraLibreModel/bitacoraLibresModel');
 const PoliticaServicios = require('../politicaServicios/politicaServicios');
 const EmpleadoServicios = require('../usuarioServicios/empleadoServicios/empleadoServicios');
 
 async function actualizarTodos(cedula_empresa) {
-  const estado = -1;
+  let estado = -1;
 
   // se llama al modelo para traer los datos con los que se van a trabajar
   try {
-    const izq = new Date('2023-3-1');
-    const der = new Date('2023-3-15');
-    await diasGeneradosRango(izq, der, cedula_empresa);
+
     const [Empleados, Libres, Politicas] = await Promise.all([
       EmpleModel.getAllByEmpresa(cedula_empresa),
       LibModel.obtenerPorEmpresa(cedula_empresa),
@@ -29,7 +28,9 @@ async function actualizarTodos(cedula_empresa) {
     // se verifica que el numero de dias asignados no se salga del limite de DECIMAL(5,2) de la BD
     verificarMaximo(nuevosDias);
     // se mandan los nuevos tiempos a la BD
-    await LibModel.actualizarTodos(nuevosDias, cedula_empresa);
+
+    estado = await LibModel.actualizarTodos(nuevosDias, cedula_empresa);
+    estado = await BitLibModel.agregarBitacorasLIB(nuevosDias, cedula_empresa);
     if (nuevosDias !== undefined) {
       return nuevosDias;
     } else {
@@ -58,6 +59,7 @@ function verificarMaximo(libres) {
   });
 };
 
+// Toma un arreglo de fechas y devuelve la fecha MIN o MAX dependiendo de la opcion
 function FechaMinMax(arregloFechas, obtenerMax) {
   if (obtenerMax === true) {
     return new Date(Math.max.apply(null, arregloFechas));
@@ -68,12 +70,13 @@ function FechaMinMax(arregloFechas, obtenerMax) {
 
 function sinActualizar(Libre, Hoy) {
   if (Libre.ultima_actualizacion !== null) {
+
     const ultimaAct = new Date(Libre.ultima_actualizacion);
     const mesUltimaAct = ultimaAct.getMonth();
     const anhoUltimaAct = ultimaAct.getFullYear();
     const mes = Hoy.getMonth();
     const anio = Hoy.getFullYear();
-    const varible = (mesUltimaAct === mes && anhoUltimaAct === anio);
+
     return !(mesUltimaAct === mes && anhoUltimaAct === anio);
   }
   else{
@@ -124,61 +127,26 @@ function asignarDias(lib, inicioVigencia, ultimaActua, maxFecha, pol){
 
 function calcularTiempos(PoliticasVigentes, LibresSinActualizar, Empleados, Hoy) {
   const nuevosDias = [];
-  // Por todas las politicas vigentes
+
   PoliticasVigentes.forEach(pol => {
     pol.fecha_final = new Date(pol.fecha_final);
     pol.fecha_inicio = new Date(pol.fecha_inicio);
-    // Se itera por todos sus libres que aun no ha sido actualizados
     LibresSinActualizar.filter(lib => lib.titulo_politica === pol.titulo).forEach(lib => {
+
       const { maxFecha, inicioVigencia, ultimaActua } = iniciarValores(Empleados, lib, pol, Hoy);
-      const diasNuevos = asignarDias(lib, inicioVigencia, ultimaActua, maxFecha, pol);
+      const diasNuevos = Number(asignarDias(lib, inicioVigencia, ultimaActua, maxFecha, pol)).toFixed(2);
 
       const libre = {
         cedula_empleado: lib.cedula_empleado,
         titulo_politica: lib.titulo_politica,
-        dias_libres_disponibles: Number(diasNuevos).toFixed(2),
-        ultima_actualizacion: Hoy.toISOString().slice(0, 10)
+        dias_libres_disponibles: diasNuevos,
+        ultima_actualizacion: Hoy.toISOString().slice(0, 10),
+        nuevos_libres: Math.abs(diasNuevos - lib.dias_libres_disponibles).toFixed(2)
       };
       nuevosDias.push(libre);
     });
   });
   return nuevosDias;
-};
-
-async function diasGeneradosRango(FechaIzq, FechaDer, cedula_empresa){
-  let estado = -1;
-  try {
-    // traer informacion de la DB con la que se va trabajar
-    let [Empleados, Libres, Politicas] = await Promise.all([
-      EmpleModel.getAllByEmpresa(cedula_empresa),
-      LibModel.obtenerPorEmpresa(cedula_empresa),
-      PolModel.getByCedulaEmpresa(cedula_empresa),
-    ]);
-    // filtrar Empleados y Politicas creados despues de Fecha pivote derecho
-    Empleados.filter(E => E.fechaContratacion <= FechaDer);
-    Politicas.filter(P => P.fecha_inicio <= FechaDer);
-    // creamos un arreglo para iterar por las politicas
-    const PoliticasArr = Politicas.reduce((acc, P) => {
-      acc[P.titulo] = P;
-      return acc;
-    }, {});
-    // se modifican libres locales para obtener dias generados en rango de fechas
-    Libres.forEach(L => {
-      const Politica = PoliticasArr[L.titulo_politica];
-      if (Politica) {
-        const fechaContratacion = EmpleadoServicios.obtenerFechaContrato(Empleados, L.cedula_empleado);
-        L.ultima_actualizacion = FechaMinMax([Politica.fecha_inicio, fechaContratacion, FechaIzq], true);
-        L.dias_libres_disponibles = 0;
-      }
-    });
-    const diasGenerados = calcularTiempos(Politicas, Libres, Empleados, FechaDer);
-    console.log(diasGenerados);
-    return diasGenerados;
-  }
-  catch(error){
-    console.error('Error al calcular dias generados en rango', error);
-    return estado;
-  }
 };
 
 async function obtenerInfoLibresPorPolitica(cedula) {
@@ -194,6 +162,7 @@ async function obtenerInfoLibresPorPolitica(cedula) {
 module.exports = {
   actualizarTodos,
   calcularTiempos,
-  obtenerInfoLibresPorPolitica,
-  diasGeneradosRango
+  obtenerInfoLibresPorPolitica
 };
+
+
--- server/servicios/politicaServicios/politicaServicios.js
+++ server/servicios/politicaServicios/politicaServicios.js
@@ -21,7 +21,6 @@ function vigente(Politica) {
 
 	const fechaInicio = new Date(Politica.fecha_inicio);
   const fechaFinal = new Date(Politica.fecha_final);
-
   return (fechaFinal >= haceUnMes && fechaInicio <= hoy
   	&& Politica.activo === true);
 }
