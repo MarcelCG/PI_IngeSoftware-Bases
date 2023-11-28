54ad322180adc3cd3701a9b7d9c27d159578de01
Elden Lord <84419673+Jeremy-0307@users.noreply.github.com>
2023-11-27 21:21:01 -0600

    Update bitacoraLibresModel.js

--- server/models/bitacoraLibreModel/bitacoraLibresModel.js
+++ server/models/bitacoraLibreModel/bitacoraLibresModel.js
@@ -14,17 +14,18 @@ async function agregarBitacorasLIB(nuevosLibres, cedula_empresa) {
       '${Lib.titulo_politica}',
       '${cedula_empresa}',
       '${Lib.nuevos_libres}',
-      '${hoy.toISOString().slice(0, 10)}'
+      '${hoy.toISOString().slice(0, 10)}',
+      '${Lib.dias_libres_disponibles}'
     )`
     );
-    console.log(valores);
     const query = `
       INSERT INTO bitacora_libres (
         cedula_empleado,
         titulo_politica,
         cedula_empresa,
         dias,
-        fecha
+        fecha,
+        total_actual
       )
       VALUES
         ${valores.join(', ')}
@@ -39,3 +40,4 @@ async function agregarBitacorasLIB(nuevosLibres, cedula_empresa) {
 module.exports = {
     agregarBitacorasLIB
 };
+ 
